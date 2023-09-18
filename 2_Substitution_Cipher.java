27032ef83009484e45e0158c8ee633d2898d33d0
Rincis Melvin M <119469262+RincisM@users.noreply.github.com>
2023-09-18 12:11:41 +0530

    Update 2_Substitution_Cipher.java

--- 2_Substitution_Cipher.java
+++ 2_Substitution_Cipher.java
@@ -1,40 +1,44 @@
 import java.util.*;
-class exercise2 {
+
+class substitutionCipher {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the String: ");
         String text = input.next();
         String subst = "";
-        for(int i = 0; i<26; i++) {
-            System.out.println("Enter a Substitution Character for "+ ((char)(65+i)) +": ");
-            char c = input.next().charAt(0);
-            if(Character.isAlphabetic(c)) {
-                subst += c;
-            }
-            else {
-                System.out.println("Enter a valid Character");
-                i--;
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.println("Enter a Substitution Character for " + c + ": ");
+            char substituted = input.next().charAt(0);
+            if (Character.isAlphabetic(substituted)) {
+                subst += substituted;
+            } else {
+                System.out.println("Enter a valid Alphabetic Character");
+                c--;
             }
         }
-        String cipher = encrypt(text, subst);
-        String normal = decrypt(cipher, subst);
-        System.out.println("The Encryted Text is: "+ cipher);
-        System.out.println("The Normal Text is (After Decryption): "+normal);
+        String cipher = encrypt(text, subst.toUpperCase());
+        String normal = decrypt(cipher, subst.toUpperCase());
+        System.out.println("The Encryted Text is: " + cipher);
+        System.out.println("The Normal Text is (After Decryption): " + normal);
         input.close();
     }
+
     static String encrypt(String text, String subst) {
+        text = text.toUpperCase();
         String cipher = "";
-        for(int i = 0; i<text.length(); i++) {
-            int a = (int)(text.charAt(i))%65;
-            cipher += subst.charAt(a);
+        for (int i = 0; i < text.length(); i++) {
+            int index = text.charAt(i) - 'A';
+            cipher += subst.charAt(index);
         }
         return cipher;
     }
+
     static String decrypt(String text, String subst) {
+        text = text.toUpperCase();
         String normal = "";
-        for(int i = 0; i<text.length(); i++) {
-            int a = (int)(text.charAt(i))%65;
-            normal += subst.charAt(a);
+        for (int i = 0; i < text.length(); i++) {
+            int index = subst.indexOf(text.charAt(i));
+            normal += (char) ('A' + index);
         }
         return normal;
     }

2acd35dbcdfc033721a4ead215e0232f997818a0
Rincis Melvin M <119469262+RincisM@users.noreply.github.com>
2023-09-18 12:11:15 +0530

    Update 1_Shift_Cipher.java

--- 1_Shift_Cipher.java
+++ 1_Shift_Cipher.java
@@ -1,39 +1,42 @@
 import java.util.*;
 
-class exercise1 {
+class caesarCipher {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the String: ");
         String text = input.next();
-        System.out.println("Enter the Key: ");
+        System.out.println("Enter the Key (1-25): ");
         int key = input.nextInt();
-        int key1 =0;
-        if(key>0) {
-            key1 = key%26;
+        if (key >= 1 && key <= 25) {
+            String cipher = encrypt(text, key);
+            System.out.println("The Cipher Text is (After Encryption): " + cipher);
+            String normal = decrypt(cipher, key);
+            System.out.println("The Normal Text After Decryption: " + normal);
+        } else {
+            System.out.println("Enter a valid key in the range 1-25");
         }
-        else {
-            System.out.println("Enter a valid Number");
-        }
-        String cipher = encrypt(text, key1);
-        System.out.println("The Cipher Text is (After Encryption): "+cipher);
-        String normal = decrypt(cipher, key1);
-        System.out.println("The Normal Text After Decryption: "+normal);
         input.close();
     }
+
     static String encrypt(String text, int key) {
-        String cipher = "";
-        for(int i = 0; i<text.length(); i++) {
-            int a = text.charAt(i);
-            cipher += (char) (a+key);
+        StringBuilder cipher = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char original = text.charAt(i);
+            if (Character.isLetter(original)) {
+                char shifted = (char) (original + key);
+                if ((Character.isLowerCase(original) && shifted > 'z')
+                        || (Character.isUpperCase(original) && shifted > 'Z')) {
+                    shifted = (char) (original - (26 - key));
+                }
+                cipher.append(shifted);
+            } else {
+                cipher.append(original);
+            }
         }
-        return cipher;
+        return cipher.toString();
     }
+
     static String decrypt(String text, int key) {
-        String cipher = "";
-        for(int i = 0; i<text.length(); i++) {
-            int a = text.charAt(i);
-            cipher += (char) (a-key);
-        }
-        return cipher;
+        return encrypt(text, 26 - key);
     }
 }

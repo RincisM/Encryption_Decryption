79f17822a060087d2db96f22ed6a759995843cc8
Rincis Melvin M <119469262+RincisM@users.noreply.github.com>
2023-09-26 11:21:15 +0530

    Updated a portion of 5_Hill_Cipher.java
    Still it is incomplete

--- 5_Hill_Cipher.java
+++ 5_Hill_Cipher.java
@@ -5,34 +5,44 @@ class hillCipher {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the String: ");
         String text = input.next();
-        System.out.println("Enter the Key (square numbers only): ");
-        String key = input.next().toUpperCase();
-        int row = 0;
-        int column = 0;
-        if(key.length()==4) {
-            row = 2;
-            column = 2;
-        } else if(key.length() == 9) {
-            row = 3;
-            column = 3;
+        if (text.length() % 2 != 0) {
+            text += 'x';
         }
-        else {
-            System.out.println("Enter the Key of length of square numbers");
-            System.exit(0);
-        }
+        String key;
+        int sq;
+        do {
+            System.out.println("Enter the Key (square numbers only): ");
+            key = input.next().toUpperCase();
+            sq = (int) Math.sqrt(key.length());
+        } while (sq * sq != key.length());
+        int row = sq;
+        int column = sq;
         int[][] keyMatrix = new int[row][column];
         int k = 0;
-        for(int i = 0; i<row; i++) {
-            for(int j = 0; j<column; j++) {
-                keyMatrix[i][j] = (key.charAt(k)) % 65;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                keyMatrix[i][j] = (key.charAt(k)) % 26;
                 k++;
             }
         }
         input.close();
     }
+
+    static int[][] createMatrix(String text, int column) {
+        int[][] matrix = new int[column][1];
+        int k = 0;
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < 1; j++) {
+                matrix[i][j] = text.charAt(k) % 65;
+                k++;
+            }
+        }
+        return matrix;
+    }
+
     static String encrypt(String text, int[][] keyMatrix) {
         String encryptedText = "";
-        
+
         return encryptedText;
     }
 }

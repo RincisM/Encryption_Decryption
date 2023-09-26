89e00c4946a69d0a34b46efc9eaa2d1354208c37
Rincis Melvin M <119469262+RincisM@users.noreply.github.com>
2023-09-26 20:19:52 +0530

    Updated 5_Hill_Cipher.java
    But not completed

--- 5_Hill_Cipher.java
+++ 5_Hill_Cipher.java
@@ -4,14 +4,11 @@ class hillCipher {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the String: ");
-        String text = input.next();
-        if (text.length() % 2 != 0) {
-            text += 'x';
-        }
+        String text = input.next().toUpperCase();
         String key;
         int sq;
         do {
-            System.out.println("Enter the Key (square numbers only): ");
+            System.out.println("Enter the Key (length of square numbers only): ");
             key = input.next().toUpperCase();
             sq = (int) Math.sqrt(key.length());
         } while (sq * sq != key.length());
@@ -21,28 +18,57 @@ class hillCipher {
         int k = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                keyMatrix[i][j] = (key.charAt(k)) % 26;
+                keyMatrix[i][j] = (key.charAt(k)) % 65;
                 k++;
             }
         }
+        String encryptedText = encrypt(text, keyMatrix, row);
+        System.out.println("The Encrypted Text is: "+ encryptedText);
         input.close();
     }
 
-    static int[][] createMatrix(String text, int column) {
-        int[][] matrix = new int[column][1];
-        int k = 0;
-        for (int i = 0; i < column; i++) {
-            for (int j = 0; j < 1; j++) {
-                matrix[i][j] = text.charAt(k) % 65;
-                k++;
-            }
+    static String[] combinationOfString(String text) {
+        int length = text.length();
+        StringBuilder newTextBuilder = new StringBuilder(text);
+        if (length % 2 != 0) {
+            newTextBuilder.append('x');
+            length++;
         }
-        return matrix;
+        String newText = newTextBuilder.toString();
+        String x[] = new String[length / 2];
+        int counter = 0;
+        for (int i = 0; i < length / 2; i++) {
+            x[i] = newText.substring(counter, counter + 2);
+            counter = counter + 2;
+        }
+        return x;
     }
 
-    static String encrypt(String text, int[][] keyMatrix) {
-        String encryptedText = "";
-
-        return encryptedText;
+    static String encrypt(String text, int[][] keyMatrix, int row) {
+        String te[] = combinationOfString(text);
+        int [][] matrix = new int[row][1];
+        int [][] newMatrix = new int[row][1];
+        StringBuilder encryptedText = new StringBuilder();
+        char t;
+        for(int i = 0; i<te.length; i++) {
+            for(int j = 0; j<2; j++) {
+                matrix[i][0] = (int)te[i].charAt(j) % 65;
+            }
+            for(int l = 0; l<row; l++) {
+                for(int m = 0; m<1; m++) {
+                    newMatrix[l][m] = 0;
+                    for(int n = 0; n<row; n++) {
+                        newMatrix[l][m] += (keyMatrix[l][n] * matrix[n][m])%26;
+                    }
+                }
+            }
+            for(int p=0; p<row; p++) {
+                for(int q = 0; q<1; q++) {
+                    t = (char) (newMatrix[p][q]+65);
+                    encryptedText.append(t);
+                }
+            }
+        }
+        return encryptedText.toString(); 
     }
 }

4d34b77ffa0dc7fa4e35bff02c04c7d98082b149
Rincis Melvin M <119469262+RincisM@users.noreply.github.com>
2023-10-23 12:05:08 +0530

    Update 8_RSA_Number.java

--- 8_RSA_Number.java
+++ 8_RSA_Number.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class RSA_Number {
     public static void main(String[] args) {
-        int d = 0, e; // e = public key
+        int d, e;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the Message: ");
         int message = input.nextInt();
@@ -11,40 +11,54 @@ class RSA_Number {
         int prime1 = input.nextInt();
         int prime2 = input.nextInt();
         input.close();
-        int n = prime1 * prime2;
-        int phiOfN = (prime1 - 1) * (prime2 -1);
-        for(e = 2; e<n; e++) {
-            if(getGCD(e, phiOfN) == 1) {
-                break;
-            }
-        }
-        System.out.println("The Public Key is: "+ e);
 
-        //Calculating the Private Key
-        for(int m = 0; m<=9; m++) {
-            int temp = 1 + (m* phiOfN);
-            if(temp % e == 0) {
-                d = temp / e;
+        int n = prime1 * prime2;
+        int phiOfN = (prime1 - 1) * (prime2 - 1);
+
+        // Calculate the public key (e)
+        for (e = 2; e < phiOfN; e++) {
+            if (getGCD(e, phiOfN) == 1) {
                 break;
             }
         }
-        System.out.println("The Private Key is: "+ d);
-        double e_message;
-        BigInteger d_message;
-        e_message = (Math.pow(message, e)) % n;
-        System.out.println("The cipher Test is: "+ e_message);
-        BigInteger bigN = BigInteger.valueOf(n);
-        BigInteger bigC = BigDecimal.valueOf(e_message).toBigInteger();
-        //decrypting the message
-        d_message = (bigC.pow(d)).mod(bigN);
-        System.out.println("The Decrypted Message is: "+d_message);
+        System.out.println("The Public Key (e) is: " + e);
+
+        // Calculate the private key (d) using the Extended Euclidean Algorithm
+        d = calculateD(e, phiOfN);
+        System.out.println("The Private Key (d) is: " + d);
+
+        // Encryption
+        BigInteger plaintext = BigInteger.valueOf(message);
+        BigInteger ciphertext = plaintext.modPow(BigInteger.valueOf(e), BigInteger.valueOf(n));
+        System.out.println("The Cipher Text is: " + ciphertext);
+
+        // Decryption
+        BigInteger decryptedMessage = ciphertext.modPow(BigInteger.valueOf(d), BigInteger.valueOf(n));
+        System.out.println("The Decrypted Message is: " + decryptedMessage);
     }
+
     public static int getGCD(int mod, int num) {
-        if(mod == 0) {
+        if (mod == 0) {
             return num;
-        }
-        else {
+        } else {
             return getGCD(num % mod, mod);
         }
+    }
+
+    public static int calculateD(int e, int phiOfN) {
+        int x = 0, y = 1, lastX = 1, lastY = 0, temp, q;
+        while (phiOfN != 0) {
+            q = e / phiOfN;
+            int remainder = e % phiOfN;
+            e = phiOfN;
+            phiOfN = remainder;
+            temp = x;
+            x = lastX - q * x;
+            lastX = temp;
+            temp = y;
+            y = lastY - q * y;
+            lastY = temp;
+        }
+        return lastX;
     }
 }
